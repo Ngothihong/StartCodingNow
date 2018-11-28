24d0f04fa4d70846aaa9541816aad4c15a223068
Truong Tien Ngoc Han <truongtienngochan97@gmail.com>
2018-11-28 11:50:35 +0700

    han1

---
+++ doan_htttdn/Areas/ADMIN/Controllers/han1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace doan_htttdn.Areas.ADMIN.Controllers
+{
+    public class han1Controller : Controller
+    {
+        // GET: ADMIN/han1
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
